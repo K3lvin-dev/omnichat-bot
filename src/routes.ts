d4d2ccec25d0ec1a625c0c086e85fd2bc8c86803
Kelvin Davila <kelvin.davila@positivo.com.br>
2025-08-18 12:33:22 -0300

    feat: remove unused movie-related routes from the router

--- src/routes.ts
+++ src/routes.ts
@@ -1,25 +1,10 @@
 import { Router } from 'express';
-import {
-  getMovieCastController,
-  getPopularMoviesController,
-  searchMovieByNameController,
-  getMovieDetailsController,
-  getSimilarMoviesController,
-  getMoviesByGenreController,
-} from '@/controllers/MovieController';
 import { chatbotController } from '@/controllers/ChatbotController';
-import { validateNumericId } from '@/middlewares/validationMiddleware';
 
 const router = Router();
 
 router.post('/chatbot', chatbotController);
 
-router.get('/movie/:id/cast', validateNumericId, getMovieCastController);
-router.get('/movies/popular', getPopularMoviesController);
-router.get('/movies/search', searchMovieByNameController);
 
-router.get('/movie/:id', validateNumericId, getMovieDetailsController);
-router.get('/movie/:id/similar', validateNumericId, getSimilarMoviesController);
-router.get('/movies/genre', getMoviesByGenreController);
 
 export { router };
